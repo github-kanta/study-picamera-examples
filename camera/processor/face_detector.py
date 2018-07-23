5aca1df89e3bff58a0e9825bfe7d06b6bde0227d
github-kanta <41506092+github-kanta@users.noreply.github.com>
2018-07-23 20:40:58 +0900

    Update face_detector.py

--- camera/processor/face_detector.py
+++ camera/processor/face_detector.py
@@ -43,6 +43,7 @@ class FaceDetector(object):
 
             font = cv2.FONT_HERSHEY_SIMPLEX
         #    cv2.putText(img,'OpenCV',(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
-            
+            cv2.putText(frame,font, 4,(255,255,255),2,cv2.LINE_AA)
+        
         # frameを戻り値として返す
         return frame
